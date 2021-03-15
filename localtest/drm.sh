2c5b246909ceef3f1ea6f6932cbe439c0e02e824
Michael Kim <michael.kim.200518@gmail.com>
2021-03-15 17:59:15 +0900

    update

---
+++ localtest/drm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm -rf /home/ubuntu/agora/localtest/node/0/.cache/*
+rm -rf /home/ubuntu/agora/localtest/node/2/.cache/*
+rm -rf /home/ubuntu/agora/localtest/node/3/.cache/*
+rm -rf /home/ubuntu/agora/localtest/node/4/.cache/*
+rm -rf /home/ubuntu/agora/localtest/node/5/.cache/*
+rm -rf /home/ubuntu/agora/localtest/node/6/.cache/*
+rm -rf /home/ubuntu/agora/localtest/node/7/.cache/*
+
+rm -rf /home/ubuntu/agora/localtest/node/0/*.lst
+rm -rf /home/ubuntu/agora/localtest/node/2/*.lst
+rm -rf /home/ubuntu/agora/localtest/node/3/*.lst
+rm -rf /home/ubuntu/agora/localtest/node/4/*.lst
+rm -rf /home/ubuntu/agora/localtest/node/5/*.lst
+rm -rf /home/ubuntu/agora/localtest/node/6/*.lst
+rm -rf /home/ubuntu/agora/localtest/node/7/*.lst
+
+rm -rf /home/ubuntu/stoa/logs
+rm -rf /home/ubuntu/stoa/data
