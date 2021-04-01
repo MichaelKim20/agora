e5bfaa805b74e904d8e0e017bec98e7d24020894
Michael Kim <michael.kim.200518@gmail.com>
2021-04-01 10:00:29 +0900

    update0

---
+++ localtest/drm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/0/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/2/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/3/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/4/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/5/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/6/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/7/.cache/*
+
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/0/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/2/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/3/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/4/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/5/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/6/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/node/7/*.lst
+
+rm -rf /Users/mukeunkim/test/stoa/logs
+rm -rf /Users/mukeunkim/test/stoa/data
+
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache0/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache2/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache3/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache4/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache5/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache6/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest/.cache7/*
---
+++ localtest1/drm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/0/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/2/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/3/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/4/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/5/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/6/.cache/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/7/.cache/*
+
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/0/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/2/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/3/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/4/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/5/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/6/*.lst
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/node/7/*.lst
+
+rm -rf /Users/mukeunkim/test/stoa/logs
+rm -rf /Users/mukeunkim/test/stoa/data
+
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache0/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache2/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache3/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache4/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache5/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache6/*
+rm -rf /Users/mukeunkim/Work/WorkSpace_BPFK/agora-group/michael20/agora/localtest1/.cache7/*
